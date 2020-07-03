3c25725febec8bfca280b3a5a61d2ca2e54314b6
xsummersx <605369747@qq.com>
2020-07-03 08:47:37 +0800

    elementui

---
+++ src/components/table.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <table></table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Table',
+  props: {
+
+  },
+  mounted () {
+
+  },
+  data () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
